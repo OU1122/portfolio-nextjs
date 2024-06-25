1392aab790360f1229a70bab16d2f9e971c57eae
OU1122 <152392720+OU1122@users.noreply.github.com>
2024-06-25 18:11:18 +0100

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,9 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
+
+import { FaWordpress } from "react-icons/fa";
+
+import { FaShopify } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import colapzImg from "@/public/colapz.png";
@@ -35,28 +38,36 @@ export const links = [
 
 export const experiencesData = [
 	{
-		title: "Graduated bootcamp",
-		location: "Miami, FL",
+		title: "Graduated College in Information Technology",
+		location: "Sumperk, Czech Republic.",
 		description:
-			"I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+			"Course with focus on Programming, Databases, and Server Management. I have gained number of relevant skills, such as:  Software Development, Database Administration and Network Security",
 		icon: React.createElement(LuGraduationCap),
-		date: "2019",
+		date: "Aug 2012 - Jun 2016",
 	},
 	{
-		title: "Front-End Developer",
-		location: "Orlando, FL",
+		title: "Web Developer",
+		location: "Prague, Czech Republic",
 		description:
-			"I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+			"Enhanced user experience and conversion rates by strategically designing landing pages and implementing UX improvements. Quickly built new commercial websites to client specifications using HTML, CSS, PHP, and JavaScript, and translated designs into robust UIs on WordPress and PrestaShop. Achieved significant conversion rate boosts through targeted landing page optimizations and UX enhancements.",
 		icon: React.createElement(CgWorkAlt),
-		date: "2019 - 2021",
+		date: "2017 - 2020",
 	},
 	{
-		title: "Full-Stack Developer",
-		location: "Houston, TX",
+		title: "E-commerce & Digital Marketing Specialist",
+		location: "London, UK",
 		description:
-			"I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-		icon: React.createElement(FaReact),
-		date: "2021 - present",
+			"At Siech Cycles, I specialised in e-commerce management, focusing on WordPress platform and leveraging my expertise in HTML, CSS, and JavaScript, while delivering on website optimisation through A/B testing and Heatmap analysis to enhance user experience and improve conversion rates.",
+		icon: React.createElement(FaWordpress),
+		date: "July 2021 - February 2023",
+	},
+	{
+		title: "Ecommerce & Digital Marketing Manager",
+		location: "London, UK",
+		description:
+			"Led the development and launch of high-performance e-commerce websites for brands like BAKBLADE, TagBand, Colapz, and Driftsun on Shopify, utilizing Liquid, HTML, CSS, and JavaScript. Enhanced website performance through SEO analysis, A/B testing, and UX monitoring, achieving a 20% improvement in page load times, a 15% reduction in bounce rates, and a 20% increase in session length. Optimized Core Web Vitals and JavaScript codebase, resulting in a 1% uplift in conversion rate and 140% growth in CM3. Collaborated with cross-functional teams to ensure seamless project execution.",
+		icon: React.createElement(FaShopify),
+		date: "2023 - present",
 	},
 ] as const;
 
@@ -66,14 +77,14 @@ export const projectsData = [
 		description:
 			"Property search made easy. Personal project built for the purpose of testing JWT authentication, new React Router and Prisma ORM",
 		tags: ["React", "TypeScript", "NodeJS", "Express", "MongoDB", "Prisma"],
-		imageUrl: corpcommentImg,
+		imageUrl: colapzImg,
 	},
 	{
 		title: "Calle Jara",
 		description:
 			"Static website created to promote a holiday rental business in Spain. With a user-friendly interface, visitors can easily learn about the property and contact the owner directly to facilitate bookings.",
 		tags: ["React", "Next.js", "Tailwind", "MUI", "Zod", "Framer Motion"],
-		imageUrl: corpcommentImg,
+		imageUrl: colapzImg,
 	},
 	{
 		title: "Colapz",
